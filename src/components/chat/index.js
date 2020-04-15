7a4e27136f04f69dde3d88c5ccdcde652675116c
Ian <ian.shane.sears@gmail.com>
2020-04-15 11:55:13 -0700

    working only on local host w/o external server

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -16,11 +16,17 @@ class Nard extends React.Component {
     }
 
     componentDidMount() {
+         // this.socket = io('https://6056e275.ngrok.io')
+        this.socket = io('http://localhost:5000')
 
-        this.socket = io('https://6056e275.ngrok.io')
         this.socket.on('message', (message) => {
-            this.setState({messages: [message, ...this.state.messages]})
-              })
+            this.setState({
+                messages: [
+                    message,
+                    ...this.state.messages
+                ]
+            })
+        })
     }
 
     sendMessage(event) {
@@ -29,7 +35,7 @@ class Nard extends React.Component {
         if (event.keyCode === 13 && body) {
             let message = {
                 body,
-                from: 'shyaboi'
+                from: 'Chat-Gang'
             }
             this.setState({
                 messages: [
@@ -44,12 +50,18 @@ class Nard extends React.Component {
 
     render() {
         return (
-            <div>shyaboi
-                <input type='text' placeholder='message here' onKeyUp={this.sendMessage}/>
-                {
+            <div>
+                <input type='text' placeholder='message here'
+                    onKeyUp={
+                        this.sendMessage
+                    }/> {
                 this.state.messages.map((message) => {
                     return (
-                        <p>{message.body}-from-{message.from}</p>
+                        <p>{
+                            message.body
+                        }  -Made By-{
+                            message.from
+                        }</p>
                     )
                 })
             } </div>
@@ -58,3 +70,58 @@ class Nard extends React.Component {
 }
 
 export default Nard;
+
+/* <input type='text' placeholder='message here' onKeyUp={this.sendMessage}/>
+
+{
+    this.state.messages.map((message) => {
+        return (
+            <p>{message.body}-from-{message.from}</p>
+        )
+    })
+    } */
+
+
+// const itemsArray = this.state.value.split(',');
+// this.setState({toDoList: itemsArray});
+// var items = this.state.toDoList.concat(itemsArray);
+// this.setState({toDoList: items})
+// event.preventDefault();
+// this.state.value = ""
+
+
+// render() {
+
+//     console.log(this.state.toDoList)
+//     const items = this.state.toDoList.map((item) => <li id='chats'>
+//         {item}</li>);
+
+//     return (
+//         <div id='chat'>
+//             <div id='grid-container'>
+//                 <label>
+
+//                     <form onSubmit={
+//                         this.handleSubmit
+//                     }>
+//                         <input type="text"
+//                             value={
+//                                 this.state.value
+//                             }
+//                             onChange={
+//                                 this.handleChange
+//                             }/>
+//                             <button type='submit' id='butt'>chat</button>
+//                     </form>
+//                     <div></div>
+
+//                 </label>
+//               </div>
+//             <div id='grid-containe2'>
+//                 <ul id='messages'>
+//                     {items}</ul>
+//             </div>
+//         </div>
+//     );
+
+// }
