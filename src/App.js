a341b8a3574a23a417c4689e9e3a8aea559ae9f6
Ian <ian.shane.sears@gmail.com>
2020-04-14 18:24:09 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nard from "../src/components/chat";
+import io from "socket.io-client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+        return (
+            <div>shyaboi
+              <Nard />
+             </div>
+        );
+    }
+
 
 export default App;
